78d2f52d8379dc79d5d4431c5eb621eafb6391de
ZachyDev <mosetizachary001@gmail.com>
2020-07-04 13:47:57 +0300

    My first vue application

--- todo-app/src/App.vue
+++ todo-app/src/App.vue
@@ -1,28 +1,24 @@
 <template>
   <div id="app">
-    <img alt="Vue logo" src="./assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+    <HelloWorld msg = "CodeWithZachy"/>
   </div>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
-
-export default {
-  name: 'App',
-  components: {
-    HelloWorld
+  import HelloWorld from './components/HelloWorld';
+  export default {
+    name: 'App',
+    components: {
+      HelloWorld
+    }
   }
-}
 </script>
 
 <style>
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
+#app{
+  color:#42b983;
+  position: relative;
+  margin-top:60px;
+
 }
 </style>
