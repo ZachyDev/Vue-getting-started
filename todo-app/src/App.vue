357d54908897ecfe3a0f0285907e1be6e0aa50e0
ZachyDev <mosetizachary001@gmail.com>
2020-07-06 12:15:08 +0300

    added axios

--- todo-app/src/App.vue
+++ todo-app/src/App.vue
@@ -11,6 +11,7 @@
   import Todos from './components/Todos';
   import Header from './components/layout/Header';
   import AddTodo from './components/AddTodo';
+  import axios from 'axios';
   export default {
     name: 'App',
     components: {
@@ -24,21 +25,7 @@
       return {
         // todo array of objects
         todos: [
-          {
-            id: 1,
-            title: 'Todo One',
-            completed: false
-          },
-           {
-            id: 2,
-            title: 'Todo Two',
-            completed: true
-          },
-           {
-            id: 3,
-            title: 'Todo Three',
-            completed: false
-          }
+          
         ]
       }
     },
@@ -49,6 +36,11 @@
       addTodo(newTodo) {
         this.todos = [...this.todos,newTodo];
       }
+    },
+    created() {
+      axios.get('https://jsonplaceholder.typicode.com/todos?_limit=10')
+        .then(res => this.todos = res.data)
+        .catch(err => console.log(err))
     }
   }
 </script>
