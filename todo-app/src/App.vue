6e3225ec2f5192846699eb887124f3fa0c202862
ZachyDev <mosetizachary001@gmail.com>
2020-07-06 12:24:19 +0300

    fetched todo data from fake online rest api

--- todo-app/src/App.vue
+++ todo-app/src/App.vue
@@ -32,13 +32,23 @@
      methods: {
       deleteTodo(id) {
         this.todos = this.todos.filter(todo => todo.id !== id);
+        // axios.delete(`https://jsonplaceholder.typicode.com/todos/${id}`)
+        this.todos = this.todos.filter(todo => todo.id !== id)
+
       },
       addTodo(newTodo) {
+        const { title,completed } = newTodo;
+        axios.post('https://jsonplaceholder.typicode.com/todos',{
+          title,
+          completed
+        })
+        .then(res => this.todos = [...this.todos,res.data])
+        .catch(err => console.log(err));
         this.todos = [...this.todos,newTodo];
       }
     },
     created() {
-      axios.get('https://jsonplaceholder.typicode.com/todos?_limit=10')
+      axios.get('https://jsonplaceholder.typicode.com/todos?_limit=5')
         .then(res => this.todos = res.data)
         .catch(err => console.log(err))
     }
